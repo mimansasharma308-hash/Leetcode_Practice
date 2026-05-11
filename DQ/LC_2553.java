f0cc7f30600e37a62521fe44e26c2b9c760b8919
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-05-11 17:13:37 +0530

    Added solution of POTD

---
+++ DQ/LC_2553.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        int n=nums.length;
+        int d=0;
+        String ans="";
+        for(int i=0;i<n;i++)
+        {
+            ans=ans+nums[i];
+        }
+        int a=ans.length();
+        int res[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            char ch=ans.charAt(i);
+            res[i]=ch-'0';
+        }
+        return res;
+    }
+}
