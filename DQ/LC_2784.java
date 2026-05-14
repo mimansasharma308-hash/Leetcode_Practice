66246b7b2374e54b1a0dfffaa8f0afa9d7284d6a
mimansasharma308-hash <mimansasharma308@gmail.com>
2026-05-14 19:02:34 +0530

    Added solution of POTD

---
+++ DQ/LC_2784.java
@@ -0,0 +1,30 @@
+class LC_2784 {
+    public boolean isGood(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int n = nums[nums.length - 1];
+        int s = nums.length;
+
+        if(s != n + 1)
+            return false;
+
+        HashMap<Integer,Integer> hash = new HashMap<>();
+
+        for(int x : nums)
+        {
+            hash.put(x, hash.getOrDefault(x,0) + 1);
+        }
+
+        for(int i = 1; i < n; i++)
+        {
+            if(hash.getOrDefault(i,0) != 1)
+                return false;
+        }
+
+        if(hash.getOrDefault(n,0) != 2)
+            return false;
+
+        return true;
+    }
+}
